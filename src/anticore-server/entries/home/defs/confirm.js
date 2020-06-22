17811287cceeb4db28de72ff67221be94b680492
Lcfvs <lcf.vs@hotmail.com>
2020-06-22 17:29:43 +0200

    Update confirm.js

--- src/anticore-server/entries/home/defs/confirm.js
+++ src/anticore-server/entries/home/defs/confirm.js
@@ -1,7 +1,7 @@
 import { fragment } from '@lcf.vs/dom-engine'
 
 export default fragment(`
-<strong>View created: <a href="{uri}">{uri}</a></strong>
+<strong>View created, restart your server and go to <a href="{uri}">{uri}</a></strong>
 `, {
   uri: null
 })
