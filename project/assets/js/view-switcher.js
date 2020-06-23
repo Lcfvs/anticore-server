79bea6e4e4ed3ebb5709d1a74744e8e175a25dfa
Lcfvs <lcf.vs@hotmail.com>
2020-06-23 16:36:47 +0200

    improving view switcher

--- project/assets/js/view-switcher.js
+++ project/assets/js/view-switcher.js
@@ -1,14 +1,39 @@
-import { on } from 'anticore'
+import { listen, on } from 'anticore'
 
-on('.anticore > main, .anticore title', (element, url) => {
-  const selector = element.nodeName.toLowerCase()
+const history = []
+
+function replace (element) {
+  const tag = element.nodeName.toLowerCase()
+  const selector = tag !== 'meta'
+    ? tag
+    : `${tag}[name="${element.name}"]`
   const current = document.querySelector(selector)
 
   current.parentNode.replaceChild(element, current)
+}
 
-  if (selector === 'title') {
-    history.pushState({}, element.innerHTML, url)
-  } else {
+on('title', (title, url) => {
+  const { parentNode } = title
+  const main = parentNode.querySelector('main')
+  const metas = [...parentNode.querySelectorAll(':scope > meta')]
+  const elements = [main, title, ...metas]
+
+  if (title.matches('body.anticore > title')) {
+    if (!main.classList.contains('error')) {
+      const state = {
+        key: history.push(elements) - 1
+      }
+
+      window.history.pushState(state, title.innerHTML, url)
+    }
+
     window.scrollTo(0, 0)
+    elements.forEach(replace)
+  } else {
+    history.push(elements)
   }
+})
+
+listen('popstate', window, ({ state: { key } }) => {
+  history[key].forEach(replace)
 })
--- readme.md
+++ readme.md
@@ -2,6 +2,26 @@
 
 **[ALPHA]** A composable DOM based template engine
 
+## Install
+
+```shell script
+mkdir anticore-server
+cd anticore-server
+git clone https://github.com/Lcfvs/anticore-server.git .
+npm i
+npm run build
+```
+
+Then go to http://localhost:8080/
+
+## Disable the generator
+
+Create an `.env` file containing this variable
+
+```shell script
+NODE_ENV=production
+
+``` 
 
 ## License
 
