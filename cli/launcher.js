f2ee9776297ccf94658a2acabb25c425d3fe0795
Lcfvs <lcf.vs@hotmail.com>
2020-06-24 18:22:20 +0200

    improving dev mode

---
+++ cli/launcher.js
@@ -0,0 +1,35 @@
+import process from 'process'
+import { spawn } from 'child_process'
+
+let child
+let exited
+
+function debug (error) {
+  console.error(error)
+  process.exit()
+}
+
+function run () {
+  if (exited) {
+    return
+  }
+
+  if (child) {
+    child.kill()
+    child = null
+  }
+
+  child = spawn('node', ['.'], {
+    stdio: 'inherit'
+  })
+
+  child.unref()
+  child.on('error', debug)
+  child.once('close', run)
+}
+
+process.once('exit', () => (exited = true))
+
+export default function launcher (compiler) {
+  compiler.once('compiled', run)
+}
